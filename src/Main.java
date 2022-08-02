e2b765af1d18800f9e6110ec72092d0b5e30eb09
Priscila Luna <priscilaramos.mk@gmail.com>
2022-08-01 22:41:56 -0300

    Mentoria e Curso

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso Java");
+        curso1.setDescricao("descrição curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso Js");
+        curso2.setDescricao("descrição curso Js");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria de Java");
+        mentoria.setDescricao("descrição mentoria Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
+    }
+}
